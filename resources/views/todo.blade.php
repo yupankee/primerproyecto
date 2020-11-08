d35e6272505effd3beda8d01992b8a452883696e
Omar Yupangui <omar.yupangui@gmail.com>
2020-11-07 23:41:49 -0500

    Creacion del proyecto

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-success">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+
+@endsection
